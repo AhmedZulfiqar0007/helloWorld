dd26fa9335b0b162fff8c14e8c9f804be40efb67
ahmed <Zulfi@terpmail.umd.edu>
2026-02-05 12:20:27 -0500

    Add Flask app with root and hello routes

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
-app = Flask(__hello_world__)
-
-
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Ahmed Zulfiqar!'
+def home():
+    return 'Welcome! Go to /hello to see the Hello World page.'
 
+@app.route('/hello')
+def hello():
+    return 'Hello World from Ahmed Zulfiqar! This is my first HTML page.'
 
 if __name__ == '__main__':
     app.run()
